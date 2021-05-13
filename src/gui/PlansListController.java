d49bc942c62dcfa97fb1c8d2a2a8f9e578c2ec57
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-05-13 11:23:20 -0300

    Botoes editar

--- src/gui/PlansListController.java
+++ src/gui/PlansListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,76 +29,71 @@ import javafx.stage.Stage;
 import model.entities.Plans;
 import model.services.PlanService;
 
-public class PlansListController implements Initializable, DataChangeListener
-{
+public class PlansListController implements Initializable, DataChangeListener {
 	private PlanService service;
-	
+
 	@FXML
 	private TableView<Plans> tableViewPlan;
-	
+
 	@FXML
 	private TableColumn<Plans, Integer> tableColumnId;
-	
+
 	@FXML
 	private TableColumn<Plans, String> tableColumnName;
-	
+
+	@FXML
+	private TableColumn<Plans, Plans> tableColumnEDIT;
+
 	@FXML
 	private Button btNew;
-	
+
 	private ObservableList<Plans> obsList;
-	
+
 	@FXML
-	public void onBtNewAction(ActionEvent event)
-	{
+	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Plans obj = new Plans();
 		createDialogForm(obj, "/gui/PlanForm.fxml", parentStage);
 	}
-	
+
 	@Override
-	public void initialize(URL url, ResourceBundle rb) 
-	{
+	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
 	}
-	
-	public void setPlanService(PlanService service)
-	{
+
+	public void setPlanService(PlanService service) {
 		this.service = service;
 	}
 
-	private void initializeNodes() 
-	{
+	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewPlan.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
-	public void updateTableView()
-	{
-		if(service==null)
-		{
+
+	public void updateTableView() {
+		if (service == null) {
 			throw new IllegalStateException("Service Null...");
 		}
 		List<Plans> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewPlan.setItems(obsList);
+		initEditButtons();
 	}
-	
-	private void createDialogForm(Plans obj, String absoluteName, Stage parentStage)
-	{
-		try
-		{
+
+	private void createDialogForm(Plans obj, String absoluteName, Stage parentStage) {
+		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
-			
+
 			PlanFormController controller = loader.getController();
 			controller.setPlan(obj);
 			controller.setPlanService(new PlanService());
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-			
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Entre com os dados do Plano/Treino");
 			dialogStage.setScene(new Scene(pane));
@@ -104,16 +101,31 @@ public class PlansListController implements Initializable, DataChangeListener
 			dialogStage.initOwner(parentStage);
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
 	@Override
-	public void onDataChanged() 
-	{
+	public void onDataChanged() {
 		updateTableView();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Plans, Plans>() {
+			private final Button button = new Button("editar");
+
+			@Override
+			protected void updateItem(Plans obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> createDialogForm(obj, "/gui/PlanForm.fxml", Utils.currentStage(event)));
+			}
+		});
 	}
 }
